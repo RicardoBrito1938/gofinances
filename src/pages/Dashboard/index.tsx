e3789c050d3c681e4230b9de1985b60237a9ed43
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2021-11-23 18:19:49 +0000

    icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon
+} from "./styles";
 
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/51454097?v=4"
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Ricardo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
